78fe79a288a2b8a6f685691711425fbfc808d36d
Shah Zafrani <szz786@gmail.com>
2015-11-16 22:16:48 -0500

    getting closer

---
+++ scripts/controllers/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Shah Zain on 11/16/2015.
+ */
+angular.module('collabriApp')
+    .controller('MainCtrl', function(){
+       this.groupMembers = [
+           'Wyatt',
+           'Allen',
+           'Garrett',
+           'Miguel',
+           'Shah'
+       ];
+    });
